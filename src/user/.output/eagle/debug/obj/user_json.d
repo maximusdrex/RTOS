27c7b763299ca71ae036e322dc75cd484b8faff0
Maxwell Schaefer <max@theschaefers.com>
2021-06-25 12:08:27 -0700

    First commit

---
+++ src/user/.output/eagle/debug/obj/user_json.d
@@ -0,0 +1,22 @@
+.output/eagle/debug/obj/user_json.o .output/eagle/debug/obj/user_json.d : user_json.c ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/osapi.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /home/max/Documents/Coding/esp/sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../include/user_config.h \
+ ../../include/os_type.h ../../include/mem.h ../include/user_json.h \
+ ../../include/json/jsonparse.h ../../include/c_types.h \
+ ../../include/json/json.h ../../include/json/jsontree.h
